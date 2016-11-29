f18b1c27cc6c543492f14de77723b67c1a77fd8d
Schmleh <fredschmleh@gmail.com>
2016-11-29 03:41:53 -0500

    Books!

---
+++ 01library/Book.java
@@ -0,0 +1,14 @@
+public class Book{
+    private String title, author, ISBN;
+    public Book(){
+	title = "";
+	author = "";
+	ISBN = "";
+    }
+    public Book(String t, String a, String I){
+	title = t;
+	author = a;
+	ISBN = I;
+    }
+    
+}
