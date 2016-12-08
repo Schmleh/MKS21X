141a56979eaf33d7d96ba88e8a1951e054b99533
Schmleh <fredschmleh@gmail.com>
2016-12-08 03:11:51 -0500

    is watching

--- 03barcode/Barcode.java
+++ 03barcode/Barcode.java
@@ -1,7 +1,10 @@
 import java.util.*;
+
 public class Barcode implements Comparable<Barcode>{
+
     private String _zip;
     private int _checkDigits;
+
     public Barcode(String zip){
 	if (zip.length() != 5){
 	    System.out.println("The barcode must be five digits long.");
@@ -17,27 +20,40 @@ public class Barcode implements Comparable<Barcode>{
 	    _checkDigits += (zip.charAt(i) - '0');
 	}
     }
+
     public Barcode clone(){
 	return new Barcode(_zip);
     }
+
     public int checkSum(){
 	return _checkDigits;
     }
+
     public String toString(){
-	String zip = _zip + _checkDigits;
+	String zip = _zip + (_checkDigits % 10);
 	String code = "|";
 	for (int i = 0; i < zip.length(); i ++){
 	    switch(zip.charAt(i) - '0'){
 	    case 1 : code += ":::||";
+		break;
 	    case 2 : code += "::|:|";
+		break;
 	    case 3 : code += "::||:";
+		break;
 	    case 4 : code += ":|::|";
+		break;
 	    case 5 : code += ":|:|:";
+		break;
 	    case 6 : code += ":||::";
+		break;
 	    case 7 : code += "|:::|";
+		break;
 	    case 8 : code += "|::|:";
+		break;
 	    case 9 : code += "|:|::";
+		break;
 	    case 0 : code += "||:::";
+		break;
 	    }
 	}
 	code += "|";
@@ -49,13 +65,13 @@ public class Barcode implements Comparable<Barcode>{
     }
 
     public static void main(String[] args){
-	/*Barcode one = new Barcode("345aa");
-	Barcode two = new Barcode("78910");
+	Barcode one = new Barcode("11100");
+	Barcode two = new Barcode("22222");
 	System.out.println(one.toString());
 	System.out.println(two.toString());
 	System.out.println(one.checkSum());
 	System.out.println(two.clone().toString());
 	System.out.println(one.compareTo(two));
-	System.out.println(one.clone());*/
+	System.out.println(one.clone());
     }
 }
